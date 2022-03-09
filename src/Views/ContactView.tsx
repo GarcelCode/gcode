4a9daf6d44e1df9c3ce58c47fc44a21a830afb45
Garceller <92178583+GarcelCode@users.noreply.github.com>
2022-03-09 11:08:50 -0600

    Favicon and Title changed

--- src/Views/ContactView.tsx
+++ src/Views/ContactView.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
       </h2>
       <article>
         {contactList.map((el) => (
-          <a key={el.id} href={el.url} target="_blank" rel="noreferrer" className="flex items-center justify-center gap-[0.5rem] text-gc-text hover:text-sky-300 mb-3 hover:scale-110 duration-300 cursor-pointer">
+          <a key={el.id} href={el.url} target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-[0.5rem] text-gc-text hover:text-sky-300 mb-3 hover:scale-110 duration-300 cursor-pointer">
             <div className="text-3xl">{el.icon}</div> <p>{el.name}</p>
           </a>
         ))}
